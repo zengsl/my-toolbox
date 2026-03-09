7698313f8aa875cc94dcc9136b9a8b0401a9ecd9
zengsl <zengsl0212@foxmail.com>
2026-03-09 18:38:12 +0800

    build: add netlify config

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import path from 'node:path';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+            imports: ['vue', 'vue-router'],
+            dts: 'src/auto-imports.d.ts',
+            eslintrc: { enabled: true },
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            dts: 'src/components.d.ts',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
